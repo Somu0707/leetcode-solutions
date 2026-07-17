00db513fecb77a32eb580b84d4849d32dd1ca071
somu <somumulugu0@gmail.com>
2026-07-17 14:05:04 +0530

    Create generate_readme.py to read C++ files
    Add script to generate README from C++ file

--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -1,1 +1,17 @@
+from utils import find_cpp_file, read_file
+import sys
 
+folder = sys.argv[1]
+
+cpp = find_cpp_file(folder)
+
+if cpp is None:
+    print("No C++ file found.")
+    exit()
+
+print("Problem Folder:", folder)
+print("CPP File:", cpp)
+
+code = read_file(cpp)
+
+print(code[:300])
